eb827ab0e00d0d15caf590a34e250a9510801a04
AndrewSC208 <meilingandrew@gmail.com>
2016-06-06 21:04:16 -0700

    Add project

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,44 @@
-import { Component } from '@angular/core';
+import { Component }       from '@angular/core';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
+
+import { HeroService }     from './hero.service';
+import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
 
 @Component({
-	selector: 'my-app',
-	template: '<h1>How fast doest this work?</h1>'
+    selector: 'my-app',
+    template:`
+        <h1>{{title}}</h1>
+        <nav>
+            <a [routerLink]="['Dashboard']">Dashboard</a>
+            <a [routerLink]="['Heroes']">Heroes</a>
+        </nav>
+        <router-outlet></router-outlet>
+    `,
+    directives: [ROUTER_DIRECTIVES],
+    providers: [
+        ROUTER_PROVIDERS,
+        HeroService
+    ]
 })
 
-export class AppComponent { }
+@RouteConfig([
+  {
+      path: '/heroes',
+      name: 'Heroes',
+      component: HeroesComponent
+  },
+  {
+      path: '/dashboard',
+      name: 'Dashboard',
+      component: DashboardComponent,
+      useAsDefault: true
+  },
+])
+
+
+export class AppComponent {
+  title = 'Tour of Heroes';
+}
+    
+PICK UP AT DASHBAORD TOP HEROES!!!!   
